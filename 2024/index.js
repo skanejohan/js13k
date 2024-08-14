0a7c06e2ab1e45e70d77af4af41b831515fcae6e
skanejohan <johan_m_ahlgren@hotmail.com>
2024-08-14 20:01:17 +0200

    Generate polygon from points. Limit movement

--- 2024/index.js
+++ 2024/index.js
@@ -24,11 +24,25 @@ document.addEventListener('keydown',
         }
     }, false);
 
+
+let points = [{x:0,y:500}, {x:500,y:500}, {x:700,y:400},  {x:800,y:200}, {x:900,y:100}, {x:1200,y:200}, {x:1500,y:200}, {x:1800,y:300}, {x:1900,y:500}, {x:2400,y:500}];
+
 // Set up the scene
+let generatePolygon = ps => {
+    let x = 0;
+    let s = '<polygon points="';
+    ps.forEach(p => {
+        s += `${p.x},${p.y} `;
+        x = p.x;
+    });
+    s += `${x},800 0,800" fill="white" />`;
+    return s;
+}
+
 layer1.innerHTML = '<rect width="200" height="100" x="100" y="100" rx="20" ry="20" fill="blue" />';
 layer2.innerHTML = '<rect width="200" height="100" x="100" y="300" rx="20" ry="20" fill="red" />';
-layer3.innerHTML = '<rect width="200" height="100" x="600" y="500" rx="20" ry="20" fill="yellow" />';
-overlay.innerHTML = '<text x="500" y="300" fill="black">WHACKY WESQUE WHEEL</text>';
+layer3.innerHTML = generatePolygon(points);
+overlay.innerHTML = '<text x="100" y="40" fill="black">WHACKY WESQUE WHEEL</text>';
 
 let lastTime = Date.now();
 
@@ -38,16 +52,26 @@ let gameLoop = () => {
 
     let translationX = Math.abs(5 * (dx * dt / 100));
     if (dx > 0) { // We move right
-        let leftToApply = rightEdge - circleTranslationX;
-        let toApply = Math.min(translationX, leftToApply);
-        circleTranslationX += toApply;
-        sceneTranslationX -= translationX - toApply;
+        if (sceneTranslationX > -1000) {
+            let leftToApply = rightEdge - circleTranslationX;
+            let toApply = Math.min(translationX, leftToApply);
+            circleTranslationX += toApply;
+            sceneTranslationX -= translationX - toApply;
+        }
+        else {
+            dx = 0;
+        }
     }
     else { // We move left
-        let leftToApply = circleTranslationX - leftEdge;
-        let toApply = Math.min(translationX, leftToApply);
-        circleTranslationX -= toApply;
-        sceneTranslationX += translationX - toApply;
+        if (sceneTranslationX < 0) {
+            let leftToApply = circleTranslationX - leftEdge;
+            let toApply = Math.min(translationX, leftToApply);
+            circleTranslationX -= toApply;
+            sceneTranslationX += translationX - toApply;
+        }
+        else {
+            dx = 0;
+        }
     }
     circle.setAttribute("transform", `translate(${circleTranslationX} ${circleTranslationY})`);
     layer3.setAttribute("transform", `translate(${sceneTranslationX} 0)`); 

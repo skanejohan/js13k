d48cd62506e280ae2456a0a4d48f1da371b3675b
skanejohan <johan_m_ahlgren@hotmail.com>
2025-08-31 19:35:59 +0200

    Icons in the menu

--- 2025/src/menu.js
+++ 2025/src/menu.js
@@ -1,10 +1,15 @@
 function displayMenu() {
     menu = svgGroup();
     menu.appendChild(svgText("TOWER OF TERROR", 400, 400));
+    menu.appendChild(svgGoodLuck(350, 500));
     menu.appendChild(svgText("the lucky number is on your side", 400, 500));
+    menu.appendChild(svgBadLuck(350, 540));
     menu.appendChild(svgText("the unlucky number is not", 400, 540));
+    menu.appendChild(svgPortal(350, 580));
     menu.appendChild(svgText("the portals... you never know where you will end up", 400, 580));
+    menu.appendChild(svgCat(350, 620));
     menu.appendChild(svgText("the black cats... your biggest fear... the horror makes you sweat and clouds your vision", 400, 620));
+    menu.appendChild(svgHorseshoe(350, 660));
     menu.appendChild(svgText("the horseshoe... your way out", 400, 660));
     menu.appendChild(svgText("escape the tower... escape the terror!", 400, 800));
     menu.appendChild(svgText("Click to start", 400, 1000, 20, "white"));
